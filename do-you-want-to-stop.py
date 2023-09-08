91c71bb4aa11f5f6fe084e3b1a567071599ddb3a
JohanUlstrup <143728768+JohanUlstrup@users.noreply.github.com>
2023-09-08 10:41:49 +0200

    sidste gang

--- do-you-want-to-stop.py
+++ do-you-want-to-stop.py
@@ -1,6 +1,7 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+""" 
 while True:
     user_input = input('Do you want to stop? ')
     if user_input.lower() == 'yes':
@@ -8,3 +9,7 @@ while True:
         break
     else:
         print('Continuing...')
+ """
+
+while input("Do you want to stop? ") !="yes":
+    None
